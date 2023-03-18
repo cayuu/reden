1af42794fd188f600d889531b38491f859f6f337
Clint Walker <cayuu@users.noreply.github.com>
2023-03-18 11:32:12 +0000

    refactor(examples): update prompt output examples

--- packages/examples/demo.ts
+++ packages/examples/demo.ts
@@ -1,4 +1,6 @@
 import reden from 'reden.ai'
 
 const p = reden.prompt('Basic prompt: [[hello]]', { hello: 'world' })
-console.log(p.toJSON())
+console.log('Prompt string:', p.toString())
+console.log(p.toObject())
+console.log(p.getParams())
